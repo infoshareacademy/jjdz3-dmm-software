8b8f634010af657ce78a1fe387ba65e96ad93996
Milo4321 <mocmil@wp.pl>
2017-01-29 21:06:30 +0100

    dodanie FileReader odczytuje pliki txt z katalogu resources, dwie metody: 1) readTxtFile - zwraca zawartość jako string (to pewnie będzie używane) 2) readTxtFileRowByRow - zwraca zawartość jako listę wierszy (może się przyda, np jeżeli byśmy chcieli kolejne wiersze traktować jako osobne obiekty JSON)

---
+++ TestProject2/src/main/java/com/test2/FileReader.java
@@ -0,0 +1,32 @@
+package com.test2;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Created by Milo on 2017-01-29.
+ */
+
+public class FileReader {
+    // zwraca całą zawartość pliku jako String
+
+    public String readTxtFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
+
+    // zwraca wiersze pliku tekstowego w List<String>
+
+    public List<String> readTxtFileRowByRow(String path, Charset encoding) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(path), encoding);
+        for (String line : lines) {
+            System.out.println(line);
+        }
+        return lines;
+
+    }
+
+}
