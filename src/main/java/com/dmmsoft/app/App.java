b0950e44636328349e75eb81d9552d8215d1c779
DanielWalendzewicz <dannwalendzewicz@gmail.com>
2017-02-23 18:24:52 +0100

    Changing Fund class to cleaner mapping files and adding method toString to represent object as a String

--- src/main/java/com/dmmsoft/app/App.java
+++ src/main/java/com/dmmsoft/app/App.java
@@ -7,19 +7,22 @@ import com.dmmsoft.app.FileIO.FilePath;
 public class App {
     public static void main(String[] args) throws Exception {
 
+
         System.out.println("Hello World!");
+
 
         // quick test of AppConfiguration (to remove)
 
         AppConfiguration appCon = new AppConfiguration("Configuration.json");
 
         System.out.println("\n*** Paths from AppConfiguration object:");
-        for(FilePath fp : appCon.getFundFilePaths()) {
+        for (FilePath fp : appCon.getFundFilePaths()) {
             System.out.println(fp.getFilePath());
         }
-        for(FilePath fp : appCon.getCurrencyFilePaths()) {
+        for (FilePath fp : appCon.getCurrencyFilePaths()) {
             System.out.println(fp.getFilePath());
         }
     }
-
 }
+
+
