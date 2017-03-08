d8a246615ed70d9cc3582ab1b7ec945a0515e6dd
Milo4321 <mocmil@wp.pl>
2017-03-08 23:33:54 +0100

    Now you can store Funds and Currences in Portfolio

---
+++ src/main/java/com/dmmsoft/app/DataLoader/PorfolioLoader.java
@@ -0,0 +1,7 @@
+package com.dmmsoft.app.DataLoader;
+
+/**
+ * Created by Admin on 2017-03-08.
+ */
+public class PorfolioLoader {
+}
---
+++ src/main/java/com/dmmsoft/app/Investment/Portfolio.java
@@ -0,0 +1,7 @@
+package com.dmmsoft.app.Investment;
+
+/**
+ * Created by Admin on 2017-03-08.
+ */
+public class Portfolio {
+}
