f1ab8148b45916b981602d1d9bbcdc37a58b357c
Milo4321 <mocmil@wp.pl>
2017-04-11 22:23:08 +0200

    Now POJOs extending Investment abstract class refactored

--- src/main/java/com/dmmsoft/app/POJO/Currency.java
+++ src/main/java/com/dmmsoft/app/POJO/Currency.java
@@ -7,57 +7,9 @@ import java.util.List;
  */
 public class Currency extends Investment {
 
-    int id;
-    String name;
-    List<Quotation> quotations;
-
-    public Currency(int id, String name, List<Quotation> quotations) {
-        super(id, name, quotations);
-        this.id = id;
-        this.name = name;
-        this.quotations = quotations;
+    public Currency(int Id, String name, List<Quotation> quotations) {
+        super(Id, name, quotations);
     }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public List<Quotation> getQuotations() {
-        return quotations;
-    }
-
-    @Override
-    public void setQuotations(List<Quotation> quotations) {
-        this.quotations = quotations;
-    }
-
-    @Override
-    public String toString() {
-        return "Currency {" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", quotations=" + quotations +
-                '}';
-    }
-
-
 
 }
 
--- src/main/java/com/dmmsoft/app/POJO/Fund.java
+++ src/main/java/com/dmmsoft/app/POJO/Fund.java
@@ -7,53 +7,9 @@ import java.util.List;
  */
 
 public class Fund extends Investment {
-    int id;
-    String name;
-    List<Quotation> quotations;
 
-    public Fund(int id, String name, List<Quotation> quotations) {
-        super(id, name, quotations);
-        this.id = id;
-        this.name = name;
-        this.quotations = quotations;
+    public Fund(int Id, String name, List<Quotation> quotations) {
+        super(Id, name, quotations);
     }
 
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public List<Quotation> getQuotations() {
-        return quotations;
-    }
-
-    @Override
-    public void setQuotations(List<Quotation> quotations) {
-        this.quotations = quotations;
-    }
-
-    @Override
-    public String toString() {
-        return "Fund {" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", quotations=" + quotations +
-                '}';
-    }
 }
