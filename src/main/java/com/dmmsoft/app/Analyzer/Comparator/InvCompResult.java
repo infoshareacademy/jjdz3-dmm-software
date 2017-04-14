4ab2252d90268eec3ee050e31d47f3224cb0572c
Milo4321 <mocmil@wp.pl>
2017-04-15 00:16:46 +0200

    Analyzer extension in progress.

---
+++ src/main/java/com/dmmsoft/app/Analyzer/Comparator/InvCompResult.java
@@ -0,0 +1,10 @@
+package com.dmmsoft.app.Analyzer.Comparator;
+
+/**
+ * Created by milo on 15.04.17.
+ */
+public class InvCompResult {
+
+
+
+}
---
+++ src/main/java/com/dmmsoft/app/Analyzer/Comparator/InvestmentComparator.java
@@ -0,0 +1,21 @@
+package com.dmmsoft.app.Analyzer.Comparator;
+
+import com.dmmsoft.app.POJO.Investment;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+/**
+ * Created by milo on 15.04.17.
+ */
+public class InvestmentComparator {
+
+
+ public InvCompResult getComparison(List<Investment> investments){
+
+     throw new NotImplementedException();
+
+ }
+
+
+}
