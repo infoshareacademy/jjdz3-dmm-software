c063cd1ef9626bd04c9dcc63357b1dc5ce9bba6c
Milo4321 <mocmil@wp.pl>
2017-04-17 23:58:30 +0200

    Just anelyzer demo file  updated

--- src/main/java/com/dmmsoft/app/Analyzer/demo.java
+++ src/main/java/com/dmmsoft/app/Analyzer/demo.java
@@ -25,41 +25,45 @@ public class demo {
 
     public static void main(String[] args) {
 
-        // test values
+        // just test values
         Double capital = 10000.00;
         DateTimeFormatter formatter = DateTimeFormatter.BASIC_ISO_DATE;
         LocalDate BUY_DATE = LocalDate.parse("20090910", formatter);
         LocalDate SELL_DATE = LocalDate.parse("20120530", formatter);
         String InvestmentName = "CHF";
 
+        // application initialization
         AppConfigurationProvider appCon = new AppConfigurationProvider().getConfiguration();
         MainContainerLoader mainContainerLoader = new MainContainerLoader(appCon);
 
+        // loading data
         mainContainerLoader.loadFunds();
         mainContainerLoader.loadCurrencies();
-
-
-        for (FilePath item : appCon.getFundFilePaths()) {
-            System.out.println(item.getFilePath());
-        }
-        for (FilePath item : appCon.getCurrencyFilePaths()) {
-            System.out.println(item.getFilePath());
-        }
-
         MainContainer mc = mainContainerLoader.getMainContainer();
 
+        // extracting investments
         List<Investment> investments = mc.getInvestments();
 
-        System.out.println("1. number of Investments loaded: " + investments.size());
-
-        // ItemStatsResult s = new ItemStats().getResult(investments, new ItemStatsQuery("AIP001"));
-
-        InvestmentRevenueQuery q = new InvestmentRevenueQuery(capital, BUY_DATE, SELL_DATE, InvestmentName);
-        InvestmentRevenureResult ir = new InvestmentRevenue(mc).getEstimatedRevenue(q);
+        // example analysis usage
+        InvestmentRevenueQuery query = new InvestmentRevenueQuery(capital, BUY_DATE, SELL_DATE, InvestmentName);
+        InvestmentRevenureResult ir = new InvestmentRevenue(mc).getEstimatedRevenue(query);
 
         System.out.println(ir.getCapitalRevenueValue());
         System.out.println(ir.getCapitalRevenueDeltaPrecentValue());
 
     }
+
+    // System.out.println("1. number of Investments loaded: " + investments.size());
+    // ItemStatsResult s = new ItemStats().getResult(investments, new ItemStatsQuery("AIP001"));
+
+    /*
+        for (FilePath item : appCon.getFundFilePaths()) {
+        System.out.println(item.getFilePath());
+    }
+        for (FilePath item : appCon.getCurrencyFilePaths()) {
+        System.out.println(item.getFilePath());
+    }*/
+
+
 
 }
