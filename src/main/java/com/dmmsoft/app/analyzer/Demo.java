d8ef239d37a755b57edb78f84cb2f8711ca46d20
Milo4321 <mocmil@wp.pl>
2017-07-05 00:22:45 +0200

    Now model data zip files (from remote source) are saved in backup folder.

--- src/main/java/com/dmmsoft/app/analyzer/Demo.java
+++ src/main/java/com/dmmsoft/app/analyzer/Demo.java
@@ -8,9 +8,11 @@ import com.dmmsoft.app.analyzer.analyses.revenue.InvestmentRevenue;
 import com.dmmsoft.app.analyzer.analyses.revenue.InvestmentRevenueCriteria;
 import com.dmmsoft.app.analyzer.analyses.revenue.InvestmentRevenueResult;
 import com.dmmsoft.app.appconfiguration.AppConfigurationProvider;
+import com.dmmsoft.app.file.RemoteDownloader;
 import com.dmmsoft.app.model.loader.MainContainerLoader;
 import com.dmmsoft.app.model.*;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -21,7 +23,7 @@ import java.util.List;
  */
 public class Demo {
 
-    public static void main(String[] args) throws NoDataForCriteria {
+    public static void main(String[] args) throws NoDataForCriteria, IOException {
 
         // just Test values (e.g. from .jsp form)
         BigDecimal capital = new BigDecimal(1512000.25);
@@ -44,6 +46,8 @@ public class Demo {
         mainContainerLoader.loadFunds();
         mainContainerLoader.loadCurrencies();
         MainContainer mc = mainContainerLoader.getMainContainer();
+
+        new RemoteDownloader().getModelFilesFromRemoteLocation();
 
         // extracting investments
         List<Investment> investments = mc.getInvestments();
--- src/main/java/com/dmmsoft/app/file/RemoteDownloader.java
+++ src/main/java/com/dmmsoft/app/file/RemoteDownloader.java
@@ -1,23 +1,66 @@
 package com.dmmsoft.app.file;
 
+import com.dmmsoft.app.analyzer.analyses.revenue.InvestmentRevenue;
 import com.dmmsoft.app.appconfiguration.AppConfigurationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by milo on 04.07.17.
  */
 public class RemoteDownloader {
 
-    @Inject
-    private AppConfigurationProvider appConfigurationProvider;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemoteDownloader.class);
+    private AppConfigurationProvider appConfigurationProvider = new AppConfigurationProvider().getConfiguration();
+    private final String DATE_PATTERN = "yyyyMMdd";
 
-    public void getModelFilesFromRemoteLocation(){
 
+    public void getModelFilesFromRemoteLocation() throws IOException {
+        try {
+            this.download(appConfigurationProvider.getCurrencyUrl().getFileUrl(),
+                    appConfigurationProvider.getCurrencyBackupFolderPath().getFolderPath());
+
+            this.download(appConfigurationProvider.getFundUrl().getFileUrl(),
+                    appConfigurationProvider.getFundBackupFolderPath().getFolderPath());
+
+        } catch (IOException ex) {
+            LOGGER.error("FAILED To download file from remote location: {}", ex.getMessage());
+        }
     }
 
+    private void download(String sourceURL, String targetDirectory) throws IOException {
+        URL url = new URL(sourceURL);
+        String fileName = sourceURL.substring(sourceURL.lastIndexOf('/') + 1, sourceURL.length());
+        Path targetPath = new File(targetDirectory
+                .concat(File.separator)
+                .concat(this.getFileNameWithDate(fileName)))
+                .toPath();
 
+        Files.copy(url.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+        LOGGER.info("File saved, source:{} target:{}",sourceURL , targetPath);
+    }
+
+    private String getFileNameWithDate(String defaultFileName) {
+
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+        String formattedDate = localDate.format(formatter);
+
+        return formattedDate
+                .concat("_")
+                .concat(defaultFileName);
+    }
 
 
 }
